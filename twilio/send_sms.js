bf3eafff10046c7f81e9a36cc8986cf42f11b7bf
QuinnCiccoretti <quinnc423@gmail.com>
2020-03-29 00:12:02 -0400

    switch to nodejs

---
+++ twilio/send_sms.js
@@ -0,0 +1,14 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Your Account Sid and Auth Token from twilio.com/console
+// DANGER! This is insecure. See http://twil.io/secure
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+12024103519',
+     to: '+15713268426'
+   })
+  .then(message => console.log(message.sid));
