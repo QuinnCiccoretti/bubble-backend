cf4c27791844a4e7fb6674f54b54cad21a65e2c5
Andrew Wang <ajwang34@gmail.com>
2020-03-29 00:23:34 -0400

    modified firebase function

--- functions/index.js
+++ functions/index.js
@@ -10,8 +10,18 @@ exports.addUser = functions.https.onRequest(async (req, res) => {
   // Grab the text parameter.
   const phone = req.query.text;
   // Push the new message into the Realtime Database using the Firebase Admin SDK.
-  const snapshot = await admin.database().ref('/Users').push({phone: phone});
+  // const snapshot = await admin.database().ref('/Users').push({phone: phone});
+
+  let snapshot = admin.database().ref('users/'+phone);
+  snapshot.set({
+    "name": "test"
+  }).then().catch();
+
   // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
   // res.redirect(303, snapshot.ref.toString());
   res.send(phone);
 });
+
+// exports.addNeighbor = functions.https.onRequest(async (req, res) => {
+
+// })
