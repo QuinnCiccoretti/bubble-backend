23687ec7f9b503b963815faa7e52eab10dfa6ea7
QuinnCiccoretti <quinnc423@gmail.com>
2020-03-29 00:41:20 -0400

    attempt twilio

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,11 @@
 const functions = require('firebase-functions');
-
 const admin = require('firebase-admin');
+
 admin.initializeApp();
+
+const accountSid = functions.config().twilio.account_sid;
+const authToken = functions.config().twilio.auth_token;
+const twilioClient = require('twilio')(accountSid, authToken);
 
 
 // Take the text parameter passed to this HTTP endpoint and insert it into the
@@ -13,5 +17,12 @@ exports.addUser = functions.https.onRequest(async (req, res) => {
   const snapshot = await admin.database().ref('/Users').push({phone: phone});
   // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
   // res.redirect(303, snapshot.ref.toString());
+  client.messages
+  .create({
+     body: 'Welcome to bubble.',
+     from: '+12024103519',
+     to: "+1"+phone
+   })
+  .then(message => console.log(message.sid));
   res.send(phone);
 });
